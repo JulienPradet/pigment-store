d0cf8ae11b52579ddf9e6534767174f963949841
Julien Pradet <julien@occitech.fr>
2016-08-14 19:11:34 +0200

    Properly set the display options visual even when the change is external

--- src/react/display/util/View/HorizontalMenu/Size.js
+++ src/react/display/util/View/HorizontalMenu/Size.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {compose, withState, withHandlers} from 'recompose'
+import {compose, lifecycle, withState, withHandlers} from 'recompose'
 import Switch from 'rc-switch'
 import './switch.css'
 import styles from './horizontalmenu.css'
@@ -7,6 +7,18 @@ import styles from './horizontalmenu.css'
 const SizeInput = compose(
   withState('auto', 'setAuto', (props) => props.value === 'auto'),
   withState('lastValue', 'setLastValue', (props) => props.value === 'auto' ? 0 : props.value),
+  lifecycle({
+    componentWillReceiveProps (props) {
+      if ((props.auto && props.value === 'auto') || (!props.auto && props.value !== 'auto')) {
+        return
+      }
+
+      props.setAuto(props.value === 'auto')
+      if (props.value !== 'auto') {
+        props.setLastValue(props.value)
+      }
+    }
+  }),
   withHandlers({
     toggle: ({name, value, onChange, lastValue, setLastValue, auto, setAuto}) => (active) => {
       if (!active) {
