d28c6ad3fdc1549b93b3fa3a765c9e1d29ee097d
Julien <julien.pradet@gmail.com>
2017-01-14 16:01:46 +0100

    Remove Slider (too big)

--- src/core/display/App/util/View/HorizontalMenu/Zoom.js
+++ src/core/display/App/util/View/HorizontalMenu/Zoom.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import classnames from 'classnames'
 import {withHandlers} from 'recompose'
-import Slider from 'rc-slider'
-import './slider.css'
 import styles from './horizontalmenu.css'
 
 const Zoom = (
@@ -16,13 +14,6 @@ const Zoom = (
     %
   </div>
   <div className={classnames(styles.zoomItem, styles.zoomSlider)}>
-    <Slider
-      min={25}
-      max={400}
-      value={parseInt(zoom)}
-      onChange={onChange}
-      tipFormatter={null}
-    />
   </div>
 </div>)
 
